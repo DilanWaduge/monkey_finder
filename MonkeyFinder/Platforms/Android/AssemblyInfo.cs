5ffec3f66dd7efc2f2e33460c02b566a6e854505
dilan waduge <dilan.waduge@motorolasolutions.com>
2025-05-15 09:56:16 +1000

    initial commit

---
+++ MonkeyFinder/Platforms/Android/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using Android;
+using Android.App;
+
+[assembly: UsesPermission(Manifest.Permission.AccessCoarseLocation)]
+[assembly: UsesPermission(Manifest.Permission.AccessFineLocation)]
+[assembly: UsesFeature("android.hardware.location", Required = false)]
+[assembly: UsesFeature("android.hardware.location.gps", Required = false)]
+[assembly: UsesFeature("android.hardware.location.network", Required = false)]
+[assembly: UsesPermission(Manifest.Permission.Internet)]
